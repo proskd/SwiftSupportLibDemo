f77ca702c3952e26655d5ef1b9ef47d09e4ebf47
David Proskin <dproskin@shutterfly.com>
2018-09-20 11:13:27 -0400

    added second framework

---
+++ SwiftSupportLibDemo/ModelFramework/ModelFramework/ModelFramework.h
@@ -0,0 +1,19 @@
+//
+//  ModelFramework.h
+//  ModelFramework
+//
+//  Created by David Proskin on 9/20/18.
+//  Copyright © 2018 Shutterfly, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModelFramework.
+FOUNDATION_EXPORT double ModelFrameworkVersionNumber;
+
+//! Project version string for ModelFramework.
+FOUNDATION_EXPORT const unsigned char ModelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelFramework/PublicHeader.h>
+
+

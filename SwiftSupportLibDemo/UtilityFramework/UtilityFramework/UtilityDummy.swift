830f9824b16ecec3bb0de67597c7c25aa06a4c92
David Proskin <dproskin@shutterfly.com>
2018-09-20 11:20:39 -0400

    create a dummy swift file that causes unit tests to break

---
+++ SwiftSupportLibDemo/UtilityFramework/UtilityFramework/UtilityDummy.swift
@@ -0,0 +1,15 @@
+//
+//  UtilityDummy.swift
+//  UtilityFramework
+//
+//  Created by David Proskin on 9/20/18.
+//  Copyright © 2018 Shutterfly, Inc. All rights reserved.
+//
+
+import Foundation
+
+@objc class UtilityDummy : NSObject {
+    public func dummyFunc() {
+        _ = ["foo", "bar"]
+    }
+}

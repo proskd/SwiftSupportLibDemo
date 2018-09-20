1603ffd9dcb7be3aa79834a292b2b5724d125fcd
David Proskin <dproskin@shutterfly.com>
2018-09-20 11:12:37 -0400

    added dummy framework

---
+++ SwiftSupportLibDemo/UtilityFramework/UtilityFramework/UtilityFramework.h
@@ -0,0 +1,19 @@
+//
+//  UtilityFramework.h
+//  UtilityFramework
+//
+//  Created by David Proskin on 9/20/18.
+//  Copyright © 2018 Shutterfly, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilityFramework.
+FOUNDATION_EXPORT double UtilityFrameworkVersionNumber;
+
+//! Project version string for UtilityFramework.
+FOUNDATION_EXPORT const unsigned char UtilityFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilityFramework/PublicHeader.h>
+
+
